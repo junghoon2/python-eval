63d12f675554996db2f3d1209165e691d8e29a94
jerry <erdia22@gmail.com>
2020-07-15 04:09:22 +0900

    git commit -a "numbers.py"

---
+++ 01.Variables/arithmetic.py
@@ -0,0 +1,13 @@
+# 숫자형 변수 선언하기 & 사용하기 
+
+n = 9
+m = 3 
+
+print(n + m)
+print(n / m)
+print(n // m)
+print(n % m)
+
+n = 2
+m = 5
+print(n ** m)
---
+++ 01.Variables/numbers.py
@@ -0,0 +1,8 @@
+# 숫자형 변수 선언하기 & 사용하기 
+
+n = 4
+m = 4.5 
+
+print(n + m)
+print(n / m)
+print(n % m)
